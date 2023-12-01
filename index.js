d121db612e18fa889503d38ba67768f65687e02c
c-wrenn <crislynwren@yahoo.com>
2023-12-01 04:47:45 -0600

    "recent changes"

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { Circle, Triangle, Square } = require("./lib/shapes")
+const { writeFile } = require("fs/promises");
+const combine = require("./lib/utils/svg-compile");
 //Use inheritance to reuse code
-
-
-
-inquirer
+let svgLogo;
+function generateLogo() {
+    inquirer
 .prompt ([
 {
     //Text for logo
 type: 'input',
 message: "Enter up to three characters for your LOGO text:",
-name: 'LOGOtext'
+name: 'logotext'
 //add validationt hat the text entered is 
 //no more than 3 characters
 },
@@ -24,37 +26,97 @@ name: 'textcolor'
 type: 'list',
 message: "What shape would you like your LOGO?",
 choices:['Triangle','Square','Circle'],
-name: 'LOGOshape'
+name: 'shape'
 },
-// {
-// type: 'input',
-// message: "What color would you like your LOGO?",
-// name: 'LOGOcolor'
-// //validate: (input) => {
-// //    const colorName = allColors.includes
-// //}
-// },
 {
 type:'input',
-message: "What color would you like the LOGO background?",
-name: 'LOGObackground',
+message: "What color would you like the shape?",
+name: 'shapecolor',
 }
+])
+.then((res) =>{
+    console.log(res)
 
+    switch (res.shape) {
+        
+        case 'Triangle': 
+            //include the code to create a triangle
+            svgLogo = new Triangle();
+            break;
+        case 'Circle': 
+            svgLogo = new Circle();
+            break;
+        case'Square':
+        svgLogo = new Square();
+            break;
+        default:
+            break;
+    }
+    svgLogo.setColor(res.shapecolor)
 
-]);
-
-
-switch (answers.shape) {
-    case 'Triangle':
-        //include the code to create a triangle
-        break;
-
-    case 'Circle':
-        break;
-    case'Square':
-        break;
-
-    default:
-        break;
+    const combine = new CombineSVG();
+    combine.setText(res.logotext, res.textcolor);
+    combine.setShape(svgLogo);
+    return writeFile("./generated/logo.svg", svg.render());
+}).then(() => {
+    console.log("Your SVG logo has been successfully generated and named logo.svg in the examples folder");
+});
 }
-module.export = shapes;
+// inquirer
+// .prompt ([
+// {
+//     //Text for logo
+// type: 'input',
+// message: "Enter up to three characters for your LOGO text:",
+// name: 'LOGOtext'
+// //add validationt hat the text entered is 
+// //no more than 3 characters
+// },
+// {// Color of logo
+// type: 'input',
+// message: "What color would you like the text?",
+// name: 'textcolor'
+
+// },
+// {//shape selection
+// type: 'list',
+// message: "What shape would you like your LOGO?",
+// choices:['Triangle','Square','Circle'],
+// name: 'shape'
+// },
+// {
+// type:'input',
+// message: "What color would you like the shape?",
+// name: 'shapecolor',
+// }
+// ])
+// .then((res) =>{
+//     console.log(res)
+// let svgLogo;
+//     switch (answers.shape) {
+//         case 'Triangle':
+//             //include the code to create a triangle
+//             svgLogo = new Triangle();
+//             break;
+//         case 'Circle':
+//             svgLogo = new Circle();
+//             break;
+//         case'Square':
+//         svgLogo = new Square();
+//             break;
+//         default:
+//             break;
+//     }
+//     svgLogo.setColor(res.shapecolor)
+
+//     const svg = new SVG();
+//     svg.setText(res.text, res.textcolor);
+//     svg.setShape(svgLogo);
+//     return writeFile("./generated/logo.svg", svg.render());
+// }).then(() => {
+//     console.log("Your SVG logo has been successfully generated and named logo.svg in the examples folder");
+// });
+// //};
+
+
+generateLogo();
