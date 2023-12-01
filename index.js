e28cddd0bda0811f1e5c7fd2740a20e512911608
c-wrenn <crislynwren@yahoo.com>
2023-12-01 04:00:40 -0600

    "older work"

--- index.js
+++ index.js
@@ -7,28 +7,37 @@ const fs = require('fs');
 inquirer
 .prompt ([
 {
+    //Text for logo
+type: 'input',
+message: "Enter up to three characters for your LOGO text:",
+name: 'LOGOtext'
+//add validationt hat the text entered is 
+//no more than 3 characters
+},
+{// Color of logo
+type: 'input',
+message: "What color would you like the text?",
+name: 'textcolor'
+
+},
+{//shape selection
 type: 'list',
 message: "What shape would you like your LOGO?",
 choices:['Triangle','Square','Circle'],
 name: 'LOGOshape'
 },
-{
-type: 'input',
-message: "What color would you like your LOGO?",
-name: 'LOGOcolor'
-validate: (input) => {
-    const colorName = allColors.includes
-}
-},
+// {
+// type: 'input',
+// message: "What color would you like your LOGO?",
+// name: 'LOGOcolor'
+// //validate: (input) => {
+// //    const colorName = allColors.includes
+// //}
+// },
 {
 type:'input',
 message: "What color would you like the LOGO background?",
 name: 'LOGObackground',
-},
-{
-type: 'input',
-message: "What color would you like the text?",
-name: 'textcolor'
 }
 
 
